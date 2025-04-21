a727524739f75e145373b93e59211c5037e4e7ee
Naharashu <130159705+Naharashu@users.noreply.github.com>
2025-04-21 08:23:09 +0300

    Update carcas_functions.js

--- examples/carcas_functions.js
+++ examples/carcas_functions.js
@@ -1,1 +1,13 @@
 import * as njs from '../src/core.js'
+
+console.log(
+  njs.carcas(2,4) // 24
+)
+
+console.log(
+  njs.carcas.voidless() // 1
+  }
+
+console.log(
+  njs.multyCarcas(2, 2) // 278.5456215128167
+  )
